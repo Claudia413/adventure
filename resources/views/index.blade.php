96b15d35437f27abfece456fb82feb997b520016
Claudia Engelsman <sally_claudia@hotmail.com>
2018-03-05 20:10:06 +0100

    Improve header with decoration, add introduction section, add new logo, change menu styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,20 +9,29 @@
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet" type="text/css">
 
   <!-- Styles -->
   <link href="{{ URL::asset('/css/adventure.css') }}" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-
-
+  <!-- Navigation desktop -->
   <nav class="navbar navbar-toggleable-sm navbar-inverse bg-transparent">
-    <a class="navbar-brand" href="#"><img src="/imgs/Logo.png" class="navbar-logo"/> Adventure</a>
+    <a class="navbar-brand" href="/">
+      <img src="/imgs/Mountains2.png" class="navbar-logo"/>
+    </a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-link" href="#">Route</a>
-        <a class="nav-link" href="#">Planning</a>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Route</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Planning</a>
+        </li>
       </ul>
     </div>
   </nav>
@@ -30,9 +39,21 @@
 
   <header class="flex-center">
     <div class="title">
-      <div class="subtitle">Discover New Zealand</div>
+      <div class="subline">
+        <span><img src="/imgs/Dashdecoleft.png" class="deco" alt="decorative dash"></span>
+          <span class="subtitle">Discover New Zealand</span>
+          <span><img src="/imgs/Dashdecoright.png" class="deco" alt="decorative dash"></span>
+        </div>
       <h1>Adventure</h1>
     </div>
   </header>
+
+  <section class="introduction">
+    <div class="text">
+      <h2>Quest</h2>
+      <p>Here comes a story of text and words and lines, and there will be much more meaning to that part than what I am writing here. This is just placeholder text about words and lines and sentences, where the real text will be awesome and cool and hopefully will fit in the design for which I still need to find a background image, edit it in Photoshop and also create the paint layour for the header. Among other things, like fill the rest of the website.</p>
+    </div>
+  </section>
+
 </body>
 </html>
